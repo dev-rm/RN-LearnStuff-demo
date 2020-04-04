1d6e5e23f854dce3640e62f088a511c60eb77fb2
dev-rm <54745323+dev-rm@users.noreply.github.com>
2020-04-04 23:07:52 +0530

    Add styles

--- app/sections/Header.js
+++ app/sections/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export class Header extends React.Component {
     constructor(props){
